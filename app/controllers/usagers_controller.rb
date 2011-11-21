3de46b9b498bd4aa0f6a2d2dd7291f7cce224cac
dominic <dominic@dominic-desktop.(none)>
2011-11-21 11:21:57 -0400

    calcul pourcentage

--- app/controllers/usagers_controller.rb
+++ app/controllers/usagers_controller.rb
@@ -21,7 +21,115 @@ class UsagersController < ApplicationController
 	@pourcentage_homme1 = (@Nbre_homme.to_f/@usager_total.to_f)*100
 	@pourcentage_homme = @pourcentage_homme1.round(2).to_s + " %"
 	
-
+	
+	@nbre_moinsde15ans = Usager.count(:conditions=>'tranche_age="mdequinze"')
+	@p_moinsde15ans1 = (@nbre_moinsde15ans.to_f/@usager_total.to_f)*100
+	@p_moinsde15ans = @p_moinsde15ans1.round(2).to_s + " %"
+	
+	@nbre_de15a20ans = Usager.count(:conditions=>'tranche_age="quinzeavingt"')
+	@p_de15a20ans1 = (@nbre_de15a20ans.to_f/@usager_total.to_f)*100
+	@p_de15a20ans = @p_de15a20ans1.round(2).to_s + " %"
+	
+	@nbre_de21a28ans= Usager.count(:conditions=>'tranche_age="vunavhuit"')
+	@p_de21a28ans1 = (@nbre_de21a28ans.to_f/@usager_total.to_f)*100
+	@p_de21a28ans = @p_de21a28ans1.round(2).to_s + " %"
+	
+	@nbre_plusde28ans = Usager.count(:conditions=>'tranche_age="plusdevhuit"')
+	@p_plusde28ans1 = (@nbre_plusde28ans.to_f/@usager_total.to_f)*100
+	@p_plusde28ans = @p_plusde28ans1.round(2).to_s + " %"
+	
+	
+	@Nbre_Collegien = Usager.count(:conditions=>'situation="college"')
+	@p_Collegien1 = (@Nbre_Collegien.to_f/@usager_total.to_f)*100
+	@p_Collegien = @p_Collegien1.round(1).to_s + " %"
+    
+    @Nbre_Lyceens = Usager.count(:conditions=>'situation="lyceen"')
+    @p_Lyceens1 = (@Nbre_Lyceens.to_f/@usager_total.to_f)*100
+    @p_Lyceens = @p_Lyceens1.round(1).to_s + " %"
+    
+    @Nbre_Etudiant = Usager.count(:conditions=>'situation="etudiant"')
+    @p_Etudiant1 = (@Nbre_Etudiant.to_f/@usager_total.to_f)*100
+    @p_Etudiant = @p_Etudiant1.round(1).to_s + " %"
+    
+    @Nbre_Stagiaire_form_prof = Usager.count(:conditions=>'situation="stagforpro"')
+    @p_Stagiaire_form_prof1 = (@Nbre_Stagiaire_form_prof.to_f/@usager_total.to_f)*100
+    @p_Stagiaire_form_prof = @p_Stagiaire_form_prof1.round(1).to_s + " %"
+    
+    @Nbre_Dem_Emploi = Usager.count(:conditions=>'situation="dem_emploi"')
+    @p_Dem_Emploi1 = (@Nbre_Dem_Emploi.to_f/@usager_total.to_f)*100
+    @p_Dem_Emploi = @p_Dem_Emploi1.round(1).to_s + " %"
+    
+    @Nbre_Salarie = Usager.count(:conditions=>'situation="salarie"')
+    @p_Salarie1 = (@Nbre_Salarie.to_f/@usager_total.to_f)*100
+    @p_Salarie = @p_Salarie1.round(1).to_s + " %"
+    
+    @Nbre_Beneficiaire_RSA = Usager.count(:conditions=>'situation="ben_rsa"')
+    @p_Beneficiaire_RSA1  = (@Nbre_Beneficiaire_RSA.to_f/@usager_total.to_f)*100
+    @p_Beneficiaire_RSA = @p_Beneficiaire_RSA1.round(1).to_s + " %"
+    
+    @Nbre_Sans_activite = Usager.count(:conditions=>'situation="sans_activite"')
+    @p_Sans_activite1  = (@Nbre_Sans_activite.to_f/@usager_total.to_f)*100
+    @p_Sans_activite = @p_Sans_activite1.round(1).to_s + " %"
+    
+    
+    @Nbre_enseignement = Usager.count(:conditions=>'domaine="enseignement"')
+	@p_enseignement1 = (@Nbre_enseignement.to_f/@usager_total.to_f)*100
+	@p_enseignement = @p_enseignement1.round(1).to_s + " %"
+    
+    @Nbre_metiers = Usager.count(:conditions=>'domaine="metiers"')
+    @p_metiers1 = (@Nbre_metiers.to_f/@usager_total.to_f)*100
+    @p_metiers = @p_metiers1.round(1).to_s + " %"
+    
+    @Nbre_emplois = Usager.count(:conditions=>'domaine="emplois"')
+    @p_emplois1 = (@Nbre_emplois.to_f/@usager_total.to_f)*100
+    @p_emplois = @p_emplois1.round(1).to_s + " %"
+    
+    @Nbre_formation = Usager.count(:conditions=>'domaine="formation"')
+    @p_formation1 = (@Nbre_formation.to_f/@usager_total.to_f)*100
+    @p_formation = @p_formation1.round(1).to_s + " %"
+    
+    @Nbre_vie_pratique  = Usager.count(:conditions=>'domaine="vie_pratique"')
+    @p_vie_pratique1 = (@Nbre_vie_pratique.to_f/@usager_total.to_f)*100
+    @p_vie_pratique = @p_vie_pratique1.round(1).to_s + " %"
+    
+    @Nbre_loisirs = Usager.count(:conditions=>'domaine="loisirs"')
+    @p_loisirs1 = (@Nbre_loisirs.to_f/@usager_total.to_f)*100
+    @p_loisirs = @p_loisirs1.round(1).to_s + " %"
+    
+    @Nbre_mobilite = Usager.count(:conditions=>'domaine="mobilite"')
+    @p_mobilite1  = (@Nbre_mobilite.to_f/@usager_total.to_f)*100
+    @p_mobilite = @p_mobilite1.round(1).to_s + " %"
+    
+    
+    @Nbre_insertion = Usager.count(:conditions=>'service="insertion"')
+	@p_insertion1 = (@Nbre_insertion.to_f/@usager_total.to_f)*100
+	@p_insertion = @p_insertion1.round(1).to_s + " %"
+    
+    @Nbre_logement = Usager.count(:conditions=>'service="logement"')
+    @p_logement1 = (@Nbre_logement.to_f/@usager_total.to_f)*100
+    @p_logement = @p_logement1.round(1).to_s + " %"
+    
+    @Nbre_mobilite = Usager.count(:conditions=>'service="mobilite"')
+    @p_mobilite1 = (@Nbre_mobilite.to_f/@usager_total.to_f)*100
+    @p_mobilite = @p_mobilite1.round(1).to_s + " %"
+    
+    @Nbre_annonce = Usager.count(:conditions=>'service="annonce"')
+    @p_annonce1 = (@Nbre_annonce.to_f/@usager_total.to_f)*100
+    @p_annonce = @p_annonce1.round(1).to_s + " %"
+    
+    @Nbre_bureautique  = Usager.count(:conditions=>'service="bureautique"')
+    @p_bureautique1 = (@Nbre_bureautique.to_f/@usager_total.to_f)*100
+    @p_bureautique = @p_bureautique1.round(1).to_s + " %"
+    
+    @Nbre_multimedia = Usager.count(:conditions=>'service="multimedia"')
+    @p_multimedia1 = (@Nbre_multimedia.to_f/@usager_total.to_f)*100
+    @p_multimedia = @p_multimedia1.round(1).to_s + " %"
+    
+    @Nbre_cvlm = Usager.count(:conditions=>'service="cvlm"')
+    @p_cvlm1  = (@Nbre_cvlm.to_f/@usager_total.to_f)*100
+    @p_cvlm = @p_cvlm1.round(1).to_s + " %"
+    
+    
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @usagers }
